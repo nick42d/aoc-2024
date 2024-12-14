769c88eb3a7805bfad9b62c0b5b0710950042045
Nick Dowsett <nickdowsett42@gmail.com>
2024-12-14 19:15:36 +0800

    Day 12 part 2, day 14 both parts

--- src/day_12.rs
+++ src/day_12.rs
@@ -6,10 +6,46 @@ use std::{
 struct Region {
     plant_type: char,
     plots: HashMap<(usize, usize), Plot>,
+    sides: usize,
+    visited_sides: HashSet<(usize, usize, Direction)>,
 }
 
 struct Plot {
     perimiters: usize,
+}
+
+#[derive(PartialEq, Default)]
+struct Sides {
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn rot_90(d: Direction) -> Direction {
+    match d {
+        Direction::Up => Direction::Right,
+        Direction::Down => Direction::Left,
+        Direction::Left => Direction::Up,
+        Direction::Right => Direction::Down,
+    }
+}
+
+fn rot_270(d: Direction) -> Direction {
+    match d {
+        Direction::Up => Direction::Left,
+        Direction::Down => Direction::Right,
+        Direction::Left => Direction::Down,
+        Direction::Right => Direction::Up,
+    }
 }
 
 fn get_plant_type(x: usize, y: usize, grid: &[Vec<char>]) -> Option<char> {
@@ -21,6 +57,105 @@ fn parse_input(s: &str) -> Vec<Vec<char>> {
         .lines()
         .map(|line| line.chars().collect())
         .collect()
+}
+
+fn check_edges(x: usize, y: usize, plant_type: char, grid: &[Vec<char>]) -> Sides {
+    let mut sides = Sides::default();
+    if y.checked_sub(1)
+        .map(|y| (x, y))
+        .and_then(|n| get_plant_type(n.0, n.1, grid))
+        != Some(plant_type)
+    {
+        sides.up = true;
+    };
+    if Some((x, y + 1)).and_then(|n| get_plant_type(n.0, n.1, grid)) != Some(plant_type) {
+        sides.down = true;
+    };
+    if x.checked_sub(1)
+        .map(|x| (x, y))
+        .and_then(|n| get_plant_type(n.0, n.1, grid))
+        != Some(plant_type)
+    {
+        sides.left = true
+    };
+    if Some((x + 1, y)).and_then(|n| get_plant_type(n.0, n.1, grid)) != Some(plant_type) {
+        sides.right = true;
+    };
+    sides
+}
+
+fn get_next_edge_position(
+    x: usize,
+    y: usize,
+    direction: Direction,
+    plant_type: char,
+    grid: &[Vec<char>],
+) -> (usize, usize, Direction) {
+    let get_offset = |x: usize, y: usize, x_offset, y_offset| {
+        y.checked_add_signed(y_offset).and_then(|y| {
+            x.checked_add_signed(x_offset)
+                .and_then(|x| get_plant_type(x, y, grid))
+        })
+    };
+
+    match direction {
+        Direction::Up => {
+            // If can turn 270deg, must turn 270deg
+            if get_offset(x, y, 0, -1) == Some(plant_type)
+                && get_offset(x, y, -1, -1) == Some(plant_type)
+            {
+                return (x - 1, y - 1, rot_270(direction));
+            }
+            // If can't go forward, must turn 90deg
+            if get_offset(x, y, 0, -1) != Some(plant_type) {
+                return (x, y, rot_90(direction));
+            }
+            // Otherwise, go forward
+            (x, y - 1, direction)
+        }
+        Direction::Down => {
+            // If can turn 270deg, must turn 270deg
+            if get_offset(x, y, 0, 1) == Some(plant_type)
+                && get_offset(x, y, 1, 1) == Some(plant_type)
+            {
+                return (x + 1, y + 1, rot_270(direction));
+            }
+            // If can't go forward, must turn 90deg
+            if get_offset(x, y, 0, 1) != Some(plant_type) {
+                return (x, y, rot_90(direction));
+            }
+            // Otherwise, go forward
+            (x, y + 1, direction)
+        }
+        Direction::Left => {
+            // If can turn 270deg, must turn 270deg
+            if get_offset(x, y, -1, 0) == Some(plant_type)
+                && get_offset(x, y, -1, 1) == Some(plant_type)
+            {
+                return (x - 1, y + 1, rot_270(direction));
+            }
+            // If can't go forward, must turn 90deg
+            if get_offset(x, y, -1, 0) != Some(plant_type) {
+                return (x, y, rot_90(direction));
+            }
+            // Otherwise, go forward
+            (x - 1, y, direction)
+        }
+        Direction::Right => {
+            // If can turn 270deg, must turn 270deg
+            if get_offset(x, y, 1, 0) == Some(plant_type)
+                && get_offset(x, y, 1, -1) == Some(plant_type)
+            {
+                return (x + 1, y - 1, rot_270(direction));
+            }
+            // If can't go forward, must turn 90deg
+            if get_offset(x, y, 1, 0) != Some(plant_type) {
+                return (x, y, rot_90(direction));
+            }
+            // Otherwise, go forward
+            (x + 1, y, direction)
+        }
+    }
 }
 
 /// Returns (number of perimeters of plot, list of unvisited neighbour
@@ -70,21 +205,53 @@ fn walk_region(
     plant_type: char,
     grid: &[Vec<char>],
     visited: &mut HashSet<(usize, usize)>,
-    regions: &mut Region,
+    region: &mut Region,
 ) {
     if visit_location(x, y, grid, visited) != Some(plant_type) {
         return;
     }
     let (perimiters, unvisited) =
         get_perimiters_and_unvisited_neighbours(x, y, plant_type, grid, visited);
-    let insert_result = regions.plots.insert((x, y), Plot { perimiters });
+    if check_edges(x, y, plant_type, grid).left
+        && !region.visited_sides.contains(&(x, y, Direction::Up))
+    {
+        let (sides, visited_sides) = walk_sides(x, y, plant_type, grid);
+        region.visited_sides.extend(visited_sides);
+        region.sides += sides;
+    }
+    let insert_result = region.plots.insert((x, y), Plot { perimiters });
     debug_assert!(insert_result.is_none());
     for (x, y) in unvisited {
-        walk_region(x, y, plant_type, grid, visited, regions);
+        walk_region(x, y, plant_type, grid, visited, region);
     }
 }
 
-fn get_total_fencing_price(s: &str) -> usize {
+// Returns number of sides, and list of all positions visited.
+fn walk_sides(
+    mut x: usize,
+    mut y: usize,
+    plant_type: char,
+    grid: &[Vec<char>],
+) -> (usize, HashSet<(usize, usize, Direction)>) {
+    // Find first left edge.
+    debug_assert!(check_edges(x, y, plant_type, grid).left);
+    let mut total_sides = 0;
+    let mut direction = Direction::Up;
+    let mut visited = HashSet::new();
+    loop {
+        if !visited.insert((x, y, direction)) {
+            break;
+        }
+        let prev_direction = direction;
+        (x, y, direction) = get_next_edge_position(x, y, direction, plant_type, grid);
+        if direction != prev_direction {
+            total_sides += 1
+        };
+    }
+    (total_sides, visited)
+}
+
+fn get_regions(s: &str) -> Vec<Region> {
     let grid = parse_input(s);
     let mut visited = HashSet::new();
     let mut regions = Vec::new();
@@ -93,14 +260,23 @@ fn get_total_fencing_price(s: &str) -> usize {
             let Some(plant_type) = get_plant_type(x, y, &grid) else {
                 continue;
             };
-            let mut next_region = Region {
-                plant_type,
-                plots: HashMap::new(),
-            };
-            walk_region(x, y, plant_type, &grid, &mut visited, &mut next_region);
-            regions.push(next_region);
+            if !visited.contains(&(x, y)) {
+                let mut next_region = Region {
+                    plant_type,
+                    plots: HashMap::new(),
+                    sides: 0,
+                    visited_sides: HashSet::new(),
+                };
+                walk_region(x, y, plant_type, &grid, &mut visited, &mut next_region);
+                regions.push(next_region);
+            }
         }
     }
+    regions
+}
+
+fn get_total_fencing_price(s: &str) -> usize {
+    let regions = get_regions(s);
     let price = regions.iter().fold(0, |acc, e| {
         let (plots, perims) = e.plots.values().fold((0, 0), |(plots, perims), e| {
             (plots + 1, perims + e.perimiters)
@@ -111,18 +287,32 @@ fn get_total_fencing_price(s: &str) -> usize {
     price
 }
 
+fn get_bulk_discounted_total_fencing_price(s: &str) -> usize {
+    let regions = get_regions(s);
+    let price = regions.iter().fold(0, |acc, e| {
+        let plots = e.plots.len();
+        let price = plots * e.sides;
+        acc + price
+    });
+    price
+}
+
 pub(crate) fn part_1(input: String) {
     println!("Total fencing price is {}", get_total_fencing_price(&input));
 }
 
 pub(crate) fn part_2(input: String) {
-    todo!()
+    println!(
+        "Total fencing price is {}",
+        get_bulk_discounted_total_fencing_price(&input)
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use crate::day_12::{
-        get_plant_type, get_total_fencing_price, parse_input, walk_region, Region,
+        get_bulk_discounted_total_fencing_price, get_plant_type, get_total_fencing_price,
+        parse_input, walk_region, Region,
     };
     use std::collections::{HashMap, HashSet};
 
@@ -145,6 +335,12 @@ VVIIICJJEE
 MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE";
+    const TEST_DATA_4: &str = "AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA";
     #[test]
     fn test_part_1_1() {
         assert_eq!(get_total_fencing_price(TEST_DATA_1), 140);
@@ -156,5 +352,13 @@ MMMISSJEEE";
     #[test]
     fn test_part_1_3() {
         assert_eq!(get_total_fencing_price(TEST_DATA_3), 1930);
+    }
+    #[test]
+    fn test_part_2_1() {
+        assert_eq!(get_bulk_discounted_total_fencing_price(TEST_DATA_1), 80);
+    }
+    #[test]
+    fn test_part_2_2() {
+        assert_eq!(get_bulk_discounted_total_fencing_price(TEST_DATA_4), 368);
     }
 }
--- src/day_14.rs
+++ src/day_14.rs
@@ -1,7 +1,270 @@
+use std::time::Duration;
+
+#[derive(Debug, PartialEq)]
+struct Robot {
+    x: usize,
+    y: usize,
+    vx: isize,
+    vy: isize,
+}
+
+#[derive(Debug)]
+struct Submap {
+    x_left: usize,
+    y_top: usize,
+    x_right: usize,
+    y_bot: usize,
+}
+
+fn parse_input(s: &str) -> Vec<Robot> {
+    s.lines()
+        .map(|line| {
+            let (p, v) = line.split_once(" ").unwrap();
+            let (px, py) = p.trim_start_matches("p=").split_once(',').unwrap();
+            let (vx, vy) = v.trim_start_matches("v=").split_once(',').unwrap();
+            Robot {
+                x: px.parse().unwrap(),
+                y: py.parse().unwrap(),
+                vx: vx.parse().unwrap(),
+                vy: vy.parse().unwrap(),
+            }
+        })
+        .collect()
+}
+
+fn move_robot(r: Robot, grid_width: usize, grid_height: usize) -> Robot {
+    debug_assert!(r.vx < grid_width as isize);
+    debug_assert!(r.vy < grid_height as isize);
+    let mut x =
+        r.x.checked_add_signed(r.vx)
+            .unwrap_or((grid_width + r.x).checked_add_signed(r.vx).unwrap());
+    let mut y =
+        r.y.checked_add_signed(r.vy)
+            .unwrap_or((grid_height + r.y).checked_add_signed(r.vy).unwrap());
+    if x >= grid_width {
+        x -= grid_width
+    }
+    if y >= grid_height {
+        y -= grid_height
+    }
+    Robot {
+        x,
+        y,
+        vx: r.vx,
+        vy: r.vy,
+    }
+}
+
+fn move_robots(robots: Vec<Robot>, grid_width: usize, grid_height: usize) -> Vec<Robot> {
+    robots
+        .into_iter()
+        .map(|robot| move_robot(robot, grid_width, grid_height))
+        .collect()
+}
+
+fn get_quadrants(grid_width: usize, grid_height: usize) -> [Submap; 4] {
+    let horiz_mid_left = (grid_width - 2) / 2;
+    let horiz_mid_right = (grid_width + 2) / 2;
+    let vert_mid_top = (grid_height - 2) / 2;
+    let vert_mid_bot = (grid_height + 2) / 2;
+    [
+        Submap {
+            x_left: 0,
+            y_top: 0,
+            x_right: horiz_mid_left,
+            y_bot: vert_mid_top,
+        },
+        Submap {
+            x_left: horiz_mid_right,
+            y_top: 0,
+            x_right: grid_width - 1,
+            y_bot: vert_mid_top,
+        },
+        Submap {
+            x_left: 0,
+            y_top: vert_mid_bot,
+            x_right: horiz_mid_left,
+            y_bot: grid_height - 1,
+        },
+        Submap {
+            x_left: horiz_mid_right,
+            y_top: vert_mid_bot,
+            x_right: grid_width - 1,
+            y_bot: grid_height - 1,
+        },
+    ]
+}
+
+fn robot_in_submap(r: &Robot, q: &Submap) -> bool {
+    r.x >= q.x_left && r.x <= q.x_right && r.y >= q.y_top && r.y <= q.y_bot
+}
+
+fn calculate_safety_factor_after_seconds(
+    s: &str,
+    grid_width: usize,
+    grid_height: usize,
+    seconds: usize,
+) -> usize {
+    let mut robots = parse_input(s);
+    for i in 0..seconds {
+        robots = move_robots(robots, grid_width, grid_height)
+    }
+    calculate_safety_factor(&robots, grid_width, grid_height)
+}
+
+fn calculate_safety_factor(robots: &[Robot], grid_width: usize, grid_height: usize) -> usize {
+    let quadrants = get_quadrants(grid_width, grid_height);
+    quadrants
+        .iter()
+        .map(|q| robots.iter().filter(|r| robot_in_submap(r, q)).count())
+        .reduce(|acc, e| acc * e)
+        .unwrap()
+}
+
+// This assumes when the christmas tree is drawn, a majority of robots are
+// located in the centre of the map.
+fn calculate_christmas_tree_factor(
+    robots: &[Robot],
+    grid_width: usize,
+    grid_height: usize,
+) -> usize {
+    let centre = Submap {
+        x_left: grid_width / 4,
+        y_top: grid_height / 4,
+        x_right: grid_width * 3 / 4,
+        y_bot: grid_height * 3 / 4,
+    };
+    robots
+        .iter()
+        .filter(|r| robot_in_submap(r, &centre))
+        .count()
+}
+
+fn display_robots(robots: &[Robot], grid_width: usize, grid_height: usize) {
+    let mut grid = vec![vec![false; grid_width]; grid_height];
+    for r in robots {
+        *grid.get_mut(r.y).and_then(|row| row.get_mut(r.x)).unwrap() = true;
+    }
+    for row in grid {
+        for c in row {
+            if c {
+                print!("▉");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
+
 pub(crate) fn part_1(input: String) {
-    todo!()
+    println!(
+        "Safety factor is {}",
+        calculate_safety_factor_after_seconds(&input, 101, 103, 100)
+    )
 }
 
 pub(crate) fn part_2(input: String) {
-    todo!()
+    let mut robots = parse_input(&input);
+    let w = 101;
+    let h = 103;
+    let mut max = (0, 0);
+    for i in 0..1000000 {
+        let factor = calculate_christmas_tree_factor(&robots, w, h);
+        if factor > max.0 {
+            max = (factor, i);
+            display_robots(&robots, w, h);
+            println!("High christmas tree factor detected {:?}", max);
+            println!("Press enter to display next");
+            std::io::stdin().read_line(&mut String::new());
+        }
+        robots = move_robots(robots, w, h);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{move_robot, move_robots, parse_input, Robot};
+    use crate::day_14::calculate_safety_factor_after_seconds;
+
+    const TEST_INPUT: &str = "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            calculate_safety_factor_after_seconds(TEST_INPUT, 11, 7, 100),
+            12
+        )
+    }
+    #[test]
+    fn test_single_move() {
+        let robot = Robot {
+            x: 2,
+            y: 4,
+            vx: 2,
+            vy: -3,
+        };
+        let robot = move_robot(robot, 11, 7);
+        assert_eq!(
+            robot,
+            Robot {
+                x: 4,
+                y: 1,
+                vx: 2,
+                vy: -3
+            }
+        )
+    }
+    #[test]
+    fn test_2_moves() {
+        let mut robot = Robot {
+            x: 2,
+            y: 4,
+            vx: 2,
+            vy: -3,
+        };
+        robot = move_robot(robot, 11, 7);
+        robot = move_robot(robot, 11, 7);
+        assert_eq!(
+            robot,
+            Robot {
+                x: 6,
+                y: 5,
+                vx: 2,
+                vy: -3
+            }
+        )
+    }
+    #[test]
+    fn test_3_moves() {
+        let mut robot = Robot {
+            x: 2,
+            y: 4,
+            vx: 2,
+            vy: -3,
+        };
+        robot = move_robot(robot, 11, 7);
+        robot = move_robot(robot, 11, 7);
+        robot = move_robot(robot, 11, 7);
+        assert_eq!(
+            robot,
+            Robot {
+                x: 8,
+                y: 2,
+                vx: 2,
+                vy: -3
+            }
+        )
+    }
 }
