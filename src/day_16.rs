3afeab24047dce92d04a4d7ecc02fe0598eb487b
Nick Dowsett <nickdowsett42@gmail.com>
2024-12-18 17:04:25 +0800

    Day16 part2

--- src/day_16.rs
+++ src/day_16.rs
@@ -37,58 +37,109 @@ fn parse_input(s: &str) -> Grid<Tile> {
     s.lines().map(|l| l.chars().map(char_to_tile)).collect()
 }
 
+#[derive(Debug)]
+enum PathType {
+    InProgress,
+    ReachedGoal,
+}
+
+#[derive(Debug)]
+enum PathFinder {
+    DeadEnd,
+    Exploring(Vec<(PathType, usize, Point, Direction, HashSet<Point>)>),
+}
+
 fn check_neighbours(
     score: usize,
     pos: Point,
     dir: Direction,
     mut points: HashSet<Point>,
     grid: &Grid<Tile>,
-    history: &mut HashMap<Point, (HashMap<Direction, (usize, HashSet<Point>)>)>,
-) -> Vec<(usize, Point, Direction, HashSet<Point>)> {
+    history: &mut HashMap<Point, (HashMap<Direction, usize>)>,
+) -> PathFinder {
     points.insert(pos);
     match history.entry(pos) {
         Entry::Occupied(mut e) => {
             let prev_visits = e.get_mut();
             match prev_visits.entry(dir) {
                 Entry::Occupied(mut e) => {
-                    let (prev_score, prev_dirs) = e.get_mut();
+                    let prev_score = e.get_mut();
+                    // If we have visited previously with same direction and lower score, stop
+                    // exploring this path.
                     if *prev_score < score {
-                        return vec![];
-                    }
-                    if *prev_score == score {
-                        prev_dirs.extend(points);
-                        return vec![];
+                        return PathFinder::DeadEnd;
                     }
                     *prev_score = score;
                 }
-                Entry::Vacant(vacant_entry) => {
-                    vacant_entry.insert((score, points.clone()));
+                Entry::Vacant(_) => {
+                    // If we have visited previously from the reverse direction, same rules as same
+                    // direction.
+                    if let Some(prev_score) = prev_visits.get_mut(&dir.rev()) {
+                        // If we have visited previously with same direction and lower score, stop
+                        // exploring this path.
+                        if *prev_score < score {
+                            return PathFinder::DeadEnd;
+                        }
+                    }
+                    // Otherwise, add a new entry.
+                    prev_visits.insert(dir, score);
                 }
             }
         }
         Entry::Vacant(vacant_entry) => {
-            vacant_entry.insert([(dir, (score, points.clone()))].into());
+            vacant_entry.insert([(dir, score)].into());
         }
     };
     let free_neighbours = [
-        (pos.move_direction(dir), score + 1, dir),
-        (pos.move_direction(dir.rot90()), score + 1001, dir.rot90()),
-        (pos.move_direction(dir.rot270()), score + 1001, dir.rot270()),
+        (
+            pos.move_direction(dir),
+            score + 1,
+            dir,
+            grid.get_cell_unchecked(pos),
+        ),
+        (
+            pos.move_direction(dir.rot90()),
+            score + 1001,
+            dir.rot90(),
+            grid.get_cell_unchecked(pos),
+        ),
+        (
+            pos.move_direction(dir.rot270()),
+            score + 1001,
+            dir.rot270(),
+            grid.get_cell_unchecked(pos),
+        ),
     ]
     .into_iter()
     .filter(|(p, ..)| !matches!(grid.get_cell_unchecked(*p), Tile::Wall));
     let mut out = vec![];
-    for (next_pos, next_score, next_dir) in free_neighbours {
-        out.extend(check_neighbours(
+    for (next_pos, next_score, next_dir, next_cell) in free_neighbours {
+        if next_cell == &Tile::End {
+            out.push((
+                PathType::ReachedGoal,
+                next_score,
+                next_pos,
+                next_dir,
+                points.clone(),
+            ));
+            continue;
+        }
+        let p = check_neighbours(
             next_score,
             next_pos,
             next_dir,
             points.clone(),
             grid,
             history,
-        ));
+        );
+        match p {
+            PathFinder::DeadEnd => (),
+            PathFinder::Exploring(vec) => {
+                out.extend(vec);
+            }
+        }
     }
-    out
+    PathFinder::Exploring(out)
 }
 
 fn solve_part_1(s: &str) -> usize {
@@ -108,70 +159,16 @@ fn solve_part_1(s: &str) -> usize {
         .get(&end_loc)
         .unwrap()
         .values()
-        .map(|(s, _)| s)
         .min_by(|a, b| a.cmp(b))
         .unwrap()
 }
-
-// fn walk_history_dfs(
-//     mut cur: Point,
-//     end: Point,
-//     history: &HashMap<Point, HashMap<Direction, (usize, HashSet<Point>)>>,
-// ) -> HashSet<Point> {
-//     let mut points = HashSet::new();
-//     let mut history_visited = history
-//         .iter()
-//         .map(|(k, v)| {
-//             (
-//                 *k,
-//                 v.keys()
-//                     .map(|v| (*v, false))
-//                     .collect::<HashMap<Direction, bool>>(),
-//             )
-//         })
-//         .collect::<HashMap<Point, HashMap<Direction, bool>>>();
-//     let mut backtrack_stack = vec![];
-//     loop {
-//         // Don't exceed cur
-//         if cur == end {
-//             if let Some(backtrack) = backtrack_stack.pop() {
-//                 cur = backtrack
-//             } else {
-//                 break;
-//             }
-//         }
-//         let mut possible_visits: Vec<_> = history_visited
-//             .get_mut(&cur)
-//             .unwrap()
-//             .iter_mut()
-//             .filter(|(_, b)| !**b)
-//             .collect();
-//         // If there are more than 1 possible visit, add a backtrack to the
-// stack.         if possible_visits.len() > 1 {
-//             backtrack_stack.push(cur);
-//         }
-//         // If there are no more possible visits, go to the next backtrack or
-// end.         if possible_visits.is_empty() {
-//             if let Some(backtrack) = backtrack_stack.pop() {
-//                 cur = backtrack;
-//                 continue;
-//             } else {
-//                 break;
-//             }
-//         }
-//         *possible_visits[0].1 = true;
-//         cur = cur.move_direction(possible_visits[0].0.rev());
-//         points.insert(cur);
-//     }
-//     points
-// }
 
 fn solve_part_2(s: &str) -> usize {
     let grid = parse_input(s);
     let start_loc = grid.find_unchecked(Tile::Start);
     let end_loc = grid.find_unchecked(Tile::End);
     let mut history = HashMap::new();
-    check_neighbours(
+    let neighbours = check_neighbours(
         0,
         start_loc,
         Direction::Right,
@@ -179,16 +176,18 @@ fn solve_part_2(s: &str) -> usize {
         &grid,
         &mut history,
     );
-    history
-        .get(&end_loc)
-        .unwrap()
-        .values()
-        .flat_map(|(_, p)| p.iter())
-        .collect::<HashSet<_>>()
-        .len()
-    // let mut track = walk_history_dfs(end_loc, start_loc, &history);
-    // print_data_and_track(&grid, &track);
-    // track.len()
+    match neighbours {
+        PathFinder::DeadEnd => todo!(),
+        PathFinder::Exploring(vec) => {
+            let min_score = vec.iter().map(|x| x.1).min().unwrap();
+            vec.iter()
+                .filter(|x| x.1 == min_score)
+                .flat_map(|x| x.4.iter())
+                .copied()
+                .collect::<HashSet<_>>()
+                .len()
+        }
+    }
 }
 
 fn print_data_and_track(m: &Grid<Tile>, h: &HashSet<Point>) {
--- src/day_18.rs
+++ src/day_18.rs
@@ -1,7 +1,86 @@
+use std::fmt::Display;
+
+use crate::utils::{Grid, Point};
+
+#[derive(Default)]
+enum Byte {
+    #[default]
+    Ok,
+    Corrupted,
+}
+
+impl Display for Byte {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Byte::Ok => write!(f, "."),
+            Byte::Corrupted => write!(f, "#"),
+        }
+    }
+}
+
+fn parse_input(s: &str) -> Vec<Point> {
+    s.lines()
+        .map(|l| {
+            let (x, y) = l.split_once(",").unwrap();
+            let x = x.parse().unwrap();
+            let y = y.parse().unwrap();
+            Point::new(x, y)
+        })
+        .collect()
+}
+
+fn populate_grid(v: Vec<Point>, width: usize, height: usize) -> Grid<Byte> {
+    let mut grid = Grid::new_with_default(width, height);
+    for p in v {
+        *grid.get_cell_unchecked_mut(p) = Byte::Corrupted;
+    }
+    grid
+}
+
+pub fn part_1_impl(s: &str) -> usize {
+    populate_grid(parse_input(s).iter().take(12).copied().collect(), 7, 7).print();
+    0
+}
+
 pub(crate) fn part_1(input: String) {
     todo!()
 }
 
 pub(crate) fn part_2(input: String) {
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_18::part_1_impl;
+
+    const TEST_DATA: &str = "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0";
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1_impl(TEST_DATA), 22)
+    }
 }
